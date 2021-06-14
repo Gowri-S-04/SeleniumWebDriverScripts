a23c243872c9e68ea622dd195004cb32cc754af7
Gowri <Gowri@DESKTOP-AF33508>
2021-06-14 12:16:09 +0530

    SeleniumWebDriver Project to GitHub

---
+++ SeleniumWebDriverScripts/src/main/java/SeleniumWebDriver/SeleniumWebDriverScripts/App.java
@@ -0,0 +1,13 @@
+package SeleniumWebDriver.SeleniumWebDriverScripts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
