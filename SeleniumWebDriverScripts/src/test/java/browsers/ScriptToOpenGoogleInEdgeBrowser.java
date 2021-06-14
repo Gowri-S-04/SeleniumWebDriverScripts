7caf10aadd4d095cb971efd45307533dbf02cf79
Gowri <Gowri@DESKTOP-AF33508>
2021-06-14 12:35:13 +0530

    Write a script to open google.com in edge browser (EdgeDriver)

--- SeleniumWebDriverScripts/src/test/java/browsers/ScriptToOpenGoogleInEdgeBrowser.java
+++ SeleniumWebDriverScripts/src/test/java/browsers/ScriptToOpenGoogleInEdgeBrowser.java
@@ -6,9 +6,9 @@ import org.openqa.selenium.edge.EdgeDriver;
 public class ScriptToOpenGoogleInEdgeBrowser {
 
 	public static void main(String[] args) {
-
 		System.setProperty("webdriver.edge.driver", "./Drivers\\msedgedriver.exe");
 		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
 		driver.close();
 	}
